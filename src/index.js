e6da7c8b72b201454adb7cede4a06be75c84a4db
HiramHudson <ewdrptufxk@outlook.com>
2024-03-21 17:23:13 +0800

    mollitia

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// @flow
+import React from 'react';
+import PrismDecorator from 'draft-js-prism';
+
+type Options = {
+  prism: Object,
+};
+
+const prismPlugin = (options: Options) => {
+  if (!options || !options.prism)
+    throw new Error(
+      'You must provide an instance of the prism.js library to draft-js-prism-plugin. See the documentation at https://github.com/withspectrum/draft-js-prism-plugin for more information!'
+    );
+  const { prism } = options;
+  return {
+    decorators: [
+      new PrismDecorator({
+        prism: prism,
+        getSyntax(block) {
+          const language = block.getData().get('language');
+          if (typeof prism.languages[language] === 'object') {
+            return language;
+          }
+          return null;
+        },
+        render({ type, children }) {
+          return (
+            <span className={`prism-token token ${type}`}>{children}</span>
+          );
+        },
+      }),
+    ],
+  };
+};
+
+export default prismPlugin;
